a7b9ac1037bf96892fab9df3b9556f6beb10bf18
adamaxy <adam996@libero.it>
2023-07-27 13:59:26 +0200

    okok

---
+++ objCpu.java
@@ -0,0 +1,26 @@
+
+public class objCpu {
+    String manufacturer = "AMD";
+
+    String CodePart = "100-100000910 WOF";
+
+    int  coreCount =8;
+    int maxMemoryGb = 128;
+    double coreClockGhz = 4.2;
+    int maxCoreClockGhz = 5;
+    int tdpW = 120;
+
+    String series = "AMD Ryzen 7";
+
+    String Socket = "AM5";
+
+    String IntegratedGraphics = "Radeon"; 
+}
+
+
+
+    //secondo voi va bene cosi ? o non lo so.
+
+
+
+}}
